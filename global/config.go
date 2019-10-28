bdd9e75ff8443ed1487656e32bd478d63a7e1121
tentone <tentone@outlook.com>
2019-10-28 11:16:23 +0000

    Configuration structure

--- global/config.go
+++ global/config.go
@@ -1,12 +1,30 @@
 package global
 
-type Config struct {
+const MSSQL string = "mssql"
+const SQLITE string = "sqlite"
+const MYSQL string = "mysql"
+const POSTGRES string = "postgres"
+
+// HTTP Server related configuration parameters
+type ServerConfig struct {
 	Address string `json:"address"`
 	AddressTLS string `json:"addressTLS"`
 	CertFileTLS string `json:"certFileTLS"`
 	KeyFileTLS string `json:"keyFileTLS"`
-	DataPath string `json:"dataPath"`
-	SqliteDatabase string `json:"sqliteDatabase"`
-	DevelopmentMode bool `json:"developmentMode"`
 }
 
+// Database access configuration
+type DatabaseConfig struct {
+	Dialect string `json:"dialect"`
+	Path string `json:"path;omitempty"` // Used for SQLITE only
+
+}
+
+// General configuration structure, containing all parameters.
+type Config struct {
+	Server ServerConfig `json:"server"`
+	Database DatabaseConfig `json:"database"`
+
+	DataPath string `json:"dataPath"`
+	DevelopmentMode bool `json:"developmentMode"`
+}
